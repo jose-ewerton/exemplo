0ebfa3f1b26b75216a61d8097e0d0ece3e73996b
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-19 20:42:42 -0300

    Att no style

--- Views/home.php
+++ Views/home.php
@@ -3,22 +3,22 @@
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="./Midias/img3.png" class="d-block w-100 img-fluid" alt="Erro de reenderização">
+            <img src="./Midias/carousel1.jpg" class="d-block w-100" style="width: 100%;" alt="Erro de reenderização">
             </div>
             <div class="carousel-item">
-            <img src="./Midias/img2.png" class="d-block w-100 img-fluid" alt="Erro de reenderização">
+            <img src="./Midias/carousel2.jpg" class="d-block w-100" style="width: 100%;" alt="Erro de reenderização">
             </div>
             <div class="carousel-item">
-            <img src="./Midias/img4.png" class="d-block w-100 img-fluid" alt="Erro de reenderização">
+            <img src="./Midias/carousel3.jpg" class="d-block w-100" style="width: 100%;" alt="Erro de reenderização">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+            <span class="visually-hidden">Anterior</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span class="visually-hidden">Próximo</span>
         </button>
         </div>
     </div> <!--Fechando o container -->
