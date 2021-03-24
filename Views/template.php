510b92765ecfbe01d8fd63f856e2c0e047356aac
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-23 22:41:27 -0300

    Testes

--- Views/template.php
+++ Views/template.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-    <link href="Formatacao/css/main.css" rel="stylesheet">
-    
+    <link rel="stylesheet" type="text/css" href="Formatacao/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="Formatacao/css/custom.css" />
     <title>Projeto MVC</title>
 </head>
 <body>
