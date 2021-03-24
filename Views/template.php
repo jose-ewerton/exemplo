85e6fd34f6a1e0769f4cd9171f429f403a343521
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-24 02:05:20 -0300

    update 24/02/2021

--- Views/template.php
+++ Views/template.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="Formatacao/css/main.css" />
-    <link rel="stylesheet" type="text/css" href="Formatacao/css/custom.css" />
     <title>Projeto MVC</title>
 </head>
 <body>
